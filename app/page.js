a5f023fe717cfd5abf0bfc53da8c5cdb8875a94d
Prashant Tiwari <prashanttiwari2585@gmail.com>
2025-06-19 23:40:56 +0530

    added components clerk convex

--- app/page.js
+++ app/page.js
@@ -1,7 +1,10 @@
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <div>Hi Prashant</div>
+    <div>Hi Prashant
+      <Button variant={'destructive'}> Confirm IT</Button>
+    </div>
   );
 }
