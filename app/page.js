08aa6650c36649c67dd62d2a9d047f613f5d25db
Prashant Tiwari <prashanttiwari2585@gmail.com>
2025-06-28 22:33:07 +0530

    landing page

--- app/page.js
+++ app/page.js
@@ -1,10 +1,62 @@
+import { Badge } from "@/components/ui/badge";
 import { Button } from "@/components/ui/button";
+import { ArrowRight } from "lucide-react";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="p-16">Hi Prashant
-      <Button variant={'destructive'}> Confirm IT</Button>
+    <div className="flex flex-col pt-16">
+      <section className="mt-20 pb-12 space-y-10 md:space-y-20 px-5">
+        <div className="container mx-auto px-4 md:px-6 text-center space-y-6">
+          <Badge variant="outline" className="bg-green-100 text-green-700">
+            Split expenses. Simplify life
+          </Badge>
+          <h1 className="gradient-title mx-auto max-w-4xl text-4xl font-bold md:text-7xl">
+            The smartest way to split bills with friends
+          </h1>
+          <p className="mx-auto max-w-[700px] text-gray-500 md:text-xl/relaxed">
+            Track shared expenses, split bills effortlessly, and settle up quickly. Never worry about who owes who again.
+          </p>
+
+          <div className="flex flex-col sm:flex-row items-center justify-center gap-4">
+            <Button asChild size={"lg"} className="bg-green-600 hover:bg-green-700 text-white">
+              <Link href="/dashboard" >
+                Get Started
+                <ArrowRight className="ml-2 h-4 w-4" />
+              </Link>
+            </Button>
+
+            <Button variant="outline" asChild size={"lg"} className="border-green-600 text-green-600 hover:bg-green-50">
+              <Link href="#how-it-works" >
+                See How It Works
+                <ArrowRight className="ml-2 h-4 w-4" />
+              </Link>
+            </Button>
+          </div>
+        </div>
+        <div className="container mx-auto max-w-5xl overflow-hidden rounded-xl shadow-xl">
+          <div className="gradient p-1 aspect-[16/9]">
+            <Image
+              src="/hero.png"
+              alt="Split Expenses Hero Image"
+              width={1280}
+              height={720}
+              className="rounded-lg  mx-auto"
+              priority
+            />
+          </div>
+        </div>
+      </section>
+
+      <section id="features" className="bg-gray-50 py-20">
+        <div className="container mx-auto px-4 md:px-6 text-center ">
+          <Badge variant="outline" className="bg-green-100 text-green-700">Features</Badge>
+          <h2>
+            Everything you need to split expenses
+          </h2>
+        </div>
+      </section>
     </div>
   );
 }
--- middleware.js
+++ middleware.js
@@ -1,6 +1,23 @@
-import { clerkMiddleware } from '@clerk/nextjs/server';
+import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
 
-export default clerkMiddleware();
+const isProtectedRoute = createRouteMatcher([
+  "/dashboard(.*)",
+  "/expenses(.*)",
+  "/groups(.*)",
+  "/settlements(.*)",
+  "/person(.*)",
+  "/contacts(.*)",
+])
+
+
+export default clerkMiddleware(async (auth, req) => {
+  const { userId } = await auth();
+
+  if (!userId && isProtectedRoute(req)) {
+    const { redirectToSignIn } = await auth();
+    return redirectToSignIn();
+  }
+});
 
 export const config = {
   matcher: [
